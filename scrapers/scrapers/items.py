874b518fe5296f29faf8db9bac03a74103325bca
Barabanov <stsbar@gmail.com>
2020-05-14 13:20:44 +0300

    WIP

--- scrapers/scrapers/items.py
+++ scrapers/scrapers/items.py
@@ -10,5 +10,11 @@ import scrapy
 
 class ScrapersItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    published = scrapy.Field()
+    easy_apply = scrapy.Field()
+    url = scrapy.Field()
+    description = scrapy.Field()
+    rating = scrapy.Field()
+    company_name = scrapy.Field()
+    company_size = scrapy.Field()
--- scrapers/scrapers/spiders/glassdoor.py
+++ scrapers/scrapers/spiders/glassdoor.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from selenium import webdriver
 
-JOB_TITLES = ["data+scientist", 'data+engineer', 'researcher', 'machine+learning+engineer', 'deep+learning',
-              'python+developer']
+from ..items import ScrapersItem
+
+JOB_TITLES = ["data+scientist"]
+#
+# JOB_TITLES = ["data+scientist", 'data+engineer', 'researcher', 'machine+learning+engineer', 'deep+learning',
+#               'python+developer']
 
 
 class GlassdoorSpider(scrapy.Spider):
@@ -14,6 +19,10 @@ class GlassdoorSpider(scrapy.Spider):
                   ]
     overall_pages = None
     current_page = 1
+
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.driver = webdriver.Firefox()
 
     def parse(self, response):
 
@@ -35,14 +44,24 @@ class GlassdoorSpider(scrapy.Spider):
         yield scrapy.Request(page_url, self.parse_pagination)
 
     def parse_details(self, response):
+        items = ScrapersItem()
         published = response.meta.get('published')
         title = response.css("div.css-17x2pwl.e11nt52q5").xpath("string()").extract_first()
         job_description = ' '.join(response.css("#JobDescriptionContainer .desc").xpath("string()").extract())
-        yield {"title": title,
-               "published": published,
-               "easy_apply": response.meta.get("easy_apply"),
-               "url": response.url,
-               'description': job_description}
+        rating = response.css(".css-1pmc6te.e11nt52q3").xpath("text()").extract_first()
+        company_name = response.css(".css-16nw49e.e11nt52q1").xpath("text()").extract_first()
+        items['rating'] = rating
+        items['title'] = title
+        items['published'] = published,
+        items['easy_apply'] = response.meta.get("easy_apply"),
+        items['url'] = response.url,
+        items['description'] = job_description
+        items['company_name'] = company_name
+        self.driver.get(response.url)
+        self.driver.find_element_by_css_selector(".css-1iqg1r5.e1eh6fgm0").click()
+        company_size = self.driver.find_elements_by_css_selector("#InfoFields .css-vugejy.es5l5kg0 span")[1].text
+        items['company_size'] = company_size
+        yield items
 
     def parse_pagination(self, response):
         print(f"\n\nParsing page #{self.current_page}\n\n")
